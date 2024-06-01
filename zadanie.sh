8e30718524b07ac081f95c8bfdb9ad2641204f0f
Jakub <dsw52682@student.dsw.edu.pl>
2024-06-01 16:28:50 +0200

    Zadania z cwiczen z dnia 01.06

---
+++ zadanie.sh
@@ -0,0 +1,176 @@
+git commit
+git commit
+---------------------
+
+show goal
+git branch bugFix
+git checkout bugFix
+------------------------------
+
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+---------------------------------
+
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+------------------------------------
+
+git checkout C4
+------------------------------------
+
+git checkout bugFix^
+-----------------------------------------
+
+git branch -f bugFix HEAD~2
+git branch -f main bugFix~3
+git branch -f main C6
+-------------------------------------------
+
+git checkout C2
+git revert C2
+git checkout C3
+git branch -f pushed C2'
+git branch -f local C1
+-----------------------------------------------
+
+git checkout C1
+git revert C3
+git checkout C3'
+git revert C4
+git revert C7
+git branch -f main C7'
+--------------------------------------------------
+
+git rebase -i overHere
+---------------------------------------------
+
+git rebase -i main
+git branch -f main C4'
+---------------------------------------
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git rebase caption main
+------------------------------------------
+
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+--------------------------------------------
+
+git checkout C1
+git tag v0
+git checkout C2
+git tag v1
+----------------------------------------------
+
+git commit
+----------------------------------------------
+
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+-----------------------------------------------
+
+git branch bugWork main^^2^
+-----------------------------------------------
+
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
+-------------------------------------------
+
+git clone
+---------------------------------------
+
+git commit
+git checkout C1
+git commit
+-------------------------
+
+git fetch
+-------------------------------------
+
+git pull
+------------------------
+
+git clone
+git fakeTeamwork 2	
+git commit
+git pull
+------------------------------
+
+git commit
+git commit
+git push
+------------------------------
+
+git clone
+git fakeTeamwork
+git commit
+git pull --rebase
+git push
+-----------------------------
+
+git reset --hard o/main
+git checkout -b feature C2
+git push origin feature
+--------------------------------
+
+git fetch
+git rebase o/main side1
+git rebase side1 side2
+git rebase side2 side3
+git rebase side3 main
+git push
+
+--------------------------
+
+git checkout main
+git pull
+git merge side1
+git merge side2
+git merge side3
+git push
+-----------------------------------
+
+git checkout -b side o/main
+git commit
+git pull -rebase
+git push
+-----------------------
+
+git push origin main
+git push origin foo
+----------------------------------
+
+git push origin main^:foo
+git push origin foo:main
+-----------------------------------
+
+git fetch origin c3:foo
+git fetch origin c6:main
+git checkout foo
+git merge main
+--------------------------------
+
+git push origin :foo
+git fetch origin :bar
+-------------------------
+
+git pull origin c3:foo
+git pull origin c2:side
